ef9e1c01e76e1ad17d15a12c8da47a4b5051784d
pravinwaklegit <pravin.wakle223@gmail.com>
2022-12-29 11:30:03 +0530

    First program

---
+++ JavaPrograms/src/basicPrograms/StringPrograms.java
@@ -0,0 +1,24 @@
+package basicPrograms;
+
+public class StringPrograms {
+	
+	static void ReverseString()
+	{
+		String s[]="Welcome to Java".split(" ");
+		String rev="";
+		
+		for(int i=s.length-1;i>=0;i--)
+		{
+		 rev=rev+s[i]+" ";
+		}
+		System.out.println("Reverse String is : "+rev);
+		
+		
+	}
+
+	public static void main(String[] args) {
+	
+		ReverseString();
+	}
+
+}
